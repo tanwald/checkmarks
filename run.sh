ea9bf7030a6b6c0f649110b7ba4dfd47d9fe553c
Paul Neulinger <paul.neulinger@tanwald.net>
2020-06-20 09:45:15 +0200

    theme tweaks;

--- run.sh
+++ run.sh
@@ -3,5 +3,10 @@
 if [ "$1" = "build" ]; then
     web-ext lint && web-ext build --overwrite-dest --ignore-files=run.sh --ignore-files=Checkmarks.iml
 else
+    if [ "$1" = "dark" ]; then
+        gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark'
+    elif [ "$1" = "light" ]; then
+        gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita'
+    fi
     web-ext run --reload --url 'about:debugging#/runtime/this-firefox'
 fi
